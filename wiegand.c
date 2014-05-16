55ed0049077b7adaf5e21131e12039bd010f1c18
E.J. Dyksen <ej@dyksen.net>
2014-05-16 11:31:04 -0400

    Rewrite to use interrupt-driven wiringPi calls
    Also, rewritten to use a single uint32_t to store the bits as they're being read in.

--- wiegand.c
+++ wiegand.c
@@ -1,9 +1,7 @@
 /*
- * Wiegand test v0.1
- * Written for Raspberry Pi
- * By Ben Kent
- * 11/08/2012
- * Based on an Arduino sketch by Daniel Smith: www.pagemac.com
+ * Wiegand API Raspberry Pi
+ * Based on previous code by Daniel Smith (www.pagemac.com) and Ben Kent (www.pidoorman.com)
+ * and, most recently, Kyle Mallory (irishjesus.wordpress.com)
  * Depends on the wiringPi library by Gordon Henterson: https://projects.drogon.net/raspberry-pi/wiringpi/
  *
  * The Wiegand interface has two data lines, DATA0 and DATA1.  These lines are normall held
@@ -15,127 +13,74 @@
  *   The Raspberry Pi GPIO pins are 3.3V, NOT 5V. Please take appropriate precautions to bring the
  *   5V Data 0 and Data 1 voltges down. I used a 330 ohm resistor and 3V3 Zenner diode for each
  *   connection. FAILURE TO DO THIS WILL PROBABLY BLOW UP THE RASPBERRY PI!
- *
- * The wiegand reader should be powered from a separate 12V supply. Connect the green wire (DATA0)
- * to the Raspberry Pi GPIO 0(SDA) pin, and the white wire (DATA1) to GPIO 1 (SCL).
- *
  */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <wiringPi.h>
+#include <time.h>
 
-#define D0_PIN 2
-#define D1_PIN 3
+#define TIMEOUT 3000000
 
-#define MAX_BITS 100                 // max number of bits
-#define WIEGAND_WAIT_TIME 300000      // time to wait for another wiegand pulse.
+static int32_t bitBag;
+static int bitIndex;
+static struct timespec lastBitTime;        // timestamp of the last bit received (used for timeouts)
 
-static unsigned char databits[MAX_BITS];    // stores all of the data bits
-static unsigned char bitCount;              // number of bits currently captured
-static unsigned int flagDone;              // goes low when data is currently being captured
-static unsigned int wiegand_counter;        // countdown until we assume there are no more bits
 
-PI_THREAD (waitForData0)
-{
-
-  (void)piHiPri (10) ;  // Set this thread to be high priority
-
-  for (;;)
-  {
-    if (waitForInterrupt (D0_PIN, -1) > 0)  // Got it
-    {
-//    printf ("0") ; fflush (stdout) ;
-    bitCount++;
-    flagDone = 0;
-    wiegand_counter = WIEGAND_WAIT_TIME;
-    }
-
-  }
+void data0Pulse() {
+    // The bit at bitIndex is already zero
+    bitIndex++;
+    clock_gettime(CLOCK_MONOTONIC, &lastBitTime);
 }
 
-PI_THREAD (waitForData1)
-{
-
-  (void)piHiPri (10) ;  // Set this thread to be high priority
-
-  for (;;)
-  {
-  if (waitForInterrupt (D1_PIN, -1) > 0)  // Got it
-    {
-//    printf ("1") ; fflush (stdout) ;
-    databits[bitCount] = 1;
-    bitCount++;
-    flagDone = 0;
-    wiegand_counter = WIEGAND_WAIT_TIME;
-    }
-  }
+void data1Pulse() {
+    // Set the bit at bitIndex (starting from the left) to 1
+    bitBag |= 1 << (31 - bitIndex);
+    bitIndex++;
+    clock_gettime(CLOCK_MONOTONIC, &lastBitTime);
 }
 
-void setup(void)
-{
-  system ("gpio edge 2 falling") ;
-  system ("gpio edge 3 falling") ;
+// Return true if we have some bits and it's been more than TIMEOUT ns
+// since we've recieved anything
+int finishedReadingBits() {
+    struct timespec now, delta;
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    delta.tv_nsec = now.tv_nsec - lastBitTime.tv_nsec;
 
-  // Setup wiringPi
-
-  wiringPiSetupSys () ;
-
-  // Fire off our interrupt handler
-  piThreadCreate (waitForData0) ;
-  piThreadCreate (waitForData1) ;
-
-  wiegand_counter = WIEGAND_WAIT_TIME;
-
-  bitCount = 0;
-
+    if (delta.tv_nsec > TIMEOUT && bitIndex) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
+void reset() {
+    bitBag = 0;
+    bitIndex = 0;
+}
 
-int main(void)
-{
+int init() {
+    reset();
 
-  setup();
+    system("gpio edge 2 falling");
+    system("gpio edge 3 falling");
 
-  unsigned char i;
+    wiringPiSetupSys();
 
-  char useSiteCode = '\0'; // determines whether or no to use 26 bit site code.
+    wiringPiISR(2, INT_EDGE_FALLING, data0Pulse);
+    wiringPiISR(3, INT_EDGE_FALLING, data1Pulse);
+}
 
+void main(void) {
+    init();
 
-  int validInput;
-  bitCount = 0;
-  int32_t uid = 0;
-
-  for (;;)
-  {
-    // This waits to make sure that there have been no more data pulses before processing data
-    if (!flagDone) {
-      if (--wiegand_counter == 0)
-        flagDone = 1;
-    }
-
-    // if we have bits and the wiegand counter reached 0
-    if (bitCount > 0 && flagDone)
-    {
-      //unsigned char i;
-      if (bitCount == 32 ) { //MiFare
-        for  (i=0; i<bitCount; ++i) {
-          uid = (uid << 1) + ( databits[i] );
+    while(1) {
+        if (finishedReadingBits()) {
+            int32_t swapped = __builtin_bswap32(bitBag);
+            printf("%08x\n", swapped);
+            reset();
+        } else {
+            usleep(5000);
         }
-        uid =  __builtin_bswap32 (uid);
-        printf( "UID (NFCID1): %08x\n", uid);
-        exit(0);
-      }
-
-      // cleanup and get ready for the next card
-      uid = 0;
-      bitCount = 0;
-      for (i=0; i<MAX_BITS; i++)
-      {
-        databits[i] = 0;
-      }
     }
-  }
-
-  return 0 ;
-
 }
